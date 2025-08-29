1febb466c65e5822f8106bc6091b58b3d594c5b7
김주홍 <gicente1@nate.com>
2025-08-29 18:03:05 +0900

    at class 2

--- Survival-Shooter/Assets/Scripts/EnemySpawner.cs
+++ Survival-Shooter/Assets/Scripts/EnemySpawner.cs
@@ -1,18 +1,61 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
     public Transform spawnPoint;
 
-    float enemySpawnTime = 0f;
+    float timer = 0f;
     float spawnInterval = 5f;
     float enemySpawnNumber = 2f;
+    float range = 30f;
+
+    private void Awake()
+    {
+
+
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > spawnInterval)
+        {
+            SpawnEnemy();
+
+            timer = 0f;
+        }
+
+    }
+
+    private void SpawnEnemy()
+    {
+        for (int i = 0; i < enemySpawnNumber; ++i)
+        {
+            if (RandomPoint(spawnPoint.position, range, out Vector3 spawnPos))
+            {
+            }
+        }
 
 
 
+    }
 
-
-
-
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Vector3 randomPoint = center + Random.insideUnitSphere * range;
+            NavMeshHit drop;
+            if (NavMesh.SamplePosition(randomPoint, out drop, 1.0f, NavMesh.AllAreas))
+            {
+                result = drop.position;
+                return true;
+            }
+        }
+        result = Vector3.zero;
+        return false;
+    }
 }
